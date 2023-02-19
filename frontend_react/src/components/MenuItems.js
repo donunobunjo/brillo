3e79fed3ddb629e40627af66d5ec3f7edfaa4991
donunobunjo <don.unobunjo@gmail.com>
2023-02-19 13:18:42 +0100

    install react router

--- frontend_react/src/components/MenuItems.js
+++ frontend_react/src/components/MenuItems.js
@@ -1,10 +1,38 @@
-import { useState } from "react";
+import { useState ,useEffect, useRef} from "react";
 import Dropdown from './Dropdown';
 
-const MenuItems = ({ items }) => {
+const MenuItems = ({ items, depthLevel }) => {
   const [dropdown, setDropdown] = useState(false);
+  
+  let ref = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+     if (dropdown && ref.current && !ref.current.contains(event.target)) {
+      setDropdown(false);
+     }
+    };
+    document.addEventListener("mousedown", handler);
+    document.addEventListener("touchstart", handler);
+    return () => {
+     // Cleanup the event listener
+     document.removeEventListener("mousedown", handler);
+     document.removeEventListener("touchstart", handler);
+    };
+   }, [dropdown]);
+
+   const onMouseEnter = () => {
+    window.innerWidth > 960 && setDropdown(true);
+   };
+   
+   const onMouseLeave = () => {
+    window.innerWidth > 960 && setDropdown(false);
+   };
   return (
-    <li className="menu-items">
+    <li className="menu-items" ref={ref}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
       {items.submenu ? (
         <>
           <button type="button" aria-haspopup="menu"
@@ -12,8 +40,10 @@ const MenuItems = ({ items }) => {
             onClick={() => setDropdown((prev) => !prev)}
           >
             {items.title}{' '}
+            {depthLevel > 0 ? <span>&raquo;</span> : <span className="arrow" />}
           </button>
-          <Dropdown submenus={items.submenu}
+          <Dropdown depthLevel={depthLevel}
+            submenus={items.submenu}
             dropdown={dropdown}
           />
         </>
